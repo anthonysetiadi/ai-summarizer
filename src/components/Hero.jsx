e9056bde9247b0776a49edbabdebba2160583eb7
Anthony <setiadi.anthony@gmail.com>
2023-10-12 16:19:44 +1100

    Updated Github link in Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
 				<img src={logo} alt='sumz_logo' className='w-28 object-contain' />
 				<button
 					type='button'
-					onClick={() => window.open('https://github.com/anthonysetiadi')}
+					onClick={() => window.open('https://github.com/anthonysetiadi/ai-summarizer')}
 					className='black_btn'
 				>
 					GitHub
